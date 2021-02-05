7cbe93583bfa76171b54496c95f854c30f350bf5
Vladimir <vladimirplotnikov371@gmail.com>
2021-02-05 21:38:21 +0200

    preloader

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./profile.module.scss";
 
 const Profile = () => {
-  return <div className={classes.Profile}>Profile</div>;
+  return <div className={classes.Profile}>Profile
+  </div>;
 };
 export default Profile;
