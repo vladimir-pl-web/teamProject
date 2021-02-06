7b0265feb5622dd9c376b5e4a487eb747558064c
Vladimir <vladimirplotnikov371@gmail.com>
2021-02-06 20:55:26 +0200

    register form basis

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,7 +1,54 @@
+import { useFormik } from "formik";
 import React from "react";
+import { Redirect, useHistory } from "react-router-dom";
+import Input from "../testComponents/c1-SuperInputText/SuperInputText";
+import SuperButton from "../testComponents/c2-SuperButton/SuperButton";
 import classes from "./register.module.scss";
 
 const Register = () => {
-  return <div className={classes.Register}>Register</div>;
+  const history = useHistory()
+  const  redirectToLogin = () => {
+      history.push(`/login`);
+  };
+  
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values));
+    },
+
+  });
+
+  return (
+    <div className={classes.Register}>
+      <h2>Register</h2>
+      <form className={classes.registerForm}>
+        <Input
+          type="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <Input
+          type="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+
+        <div className={classes.Btns}>
+          <SuperButton type={"submit"} onClick={() => formik.handleSubmit()}>
+            Register
+          </SuperButton>
+          <SuperButton onClick={redirectToLogin} color="blue">
+            Login
+          </SuperButton>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default Register;
