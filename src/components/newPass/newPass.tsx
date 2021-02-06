784d91865d0ebfab4b29210636b4a026b60d7d98
Pharmman <pharm.sale777@gmail.com>
2021-02-06 11:47:38 +0300

    Made components, added formik

--- src/components/newPass/newPass.tsx
+++ src/components/newPass/newPass.tsx
@@ -1,7 +1,44 @@
 import React from "react";
 import classes from "./newPass.module.scss";
+import {useFormik} from 'formik';
 
-const NewPass = () => {
-  return <div className={classes.NewPass}>NewPass</div>;
+type FormikErrorType = {
+  password?: string
+}
+
+const NewPass:React.FC = () => {
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+    },
+    validate: (values: FormikErrorType) => {
+      const errors: FormikErrorType = {};
+      if (!values.password) {
+        errors.password = 'Required';
+      } else if (values.password.length < 7) {
+        errors.password = 'Must be 7 characters or more';
+      }
+      return errors
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values))
+    },
+  });
+  return <div className={classes.NewPass}>
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <input
+            id="password"
+            type="password"
+            placeholder={'New Password'}
+            {...formik.getFieldProps('password')}
+        />
+      </div>
+      {formik.touched.password && formik.errors.password ? (
+          <div style={{color: 'red'}}>{formik.errors.password}</div>
+      ) : null}
+      <button type="submit">Login</button>
+    </form>
+  </div>;
 };
 export default NewPass;
--- src/components/passRecower/recover.tsx
+++ src/components/passRecower/recover.tsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import classes from './passRecover.module.scss'
+import {useFormik} from 'formik';
 
+type FormikErrorType = {
+    email?: string
+}
 
-const PassRecover = () => {
-  return (
-    <div className={classes.PassRecover}>
-     Recover
-    </div>
-  )
+const PassRecover: React.FC = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+        },
+        validate: (values: FormikErrorType) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            return errors
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values))
+        },
+    });
+    return (
+        <div className={classes.PassRecover}>
+            RECOVER
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                <input
+                    id="email"
+                    type="email"
+                    placeholder={'Email'}
+                    {...formik.getFieldProps('email')}
+                />
+                </div>
+                {formik.touched.email && formik.errors.email ? (
+                    <div style={{color: 'red'}}>{formik.errors.email}</div>
+                ) : null}
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
 }
 export default PassRecover
