9785a99718f116999fbbaf685204dc3e4f93f6ae
alx8437 <dezion777@gmail.com>
2021-02-10 23:57:31 +0300

    transfer passwordApi in common api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -16,8 +16,18 @@ type LoginResponseType = {
     error?: string;
 }
 
+const messageForRecoveryPassword =
+    `<div>
+       To recover your password, follow the link:
+        <br/> 
+          <a href='http://localhost:3000/teamProject#/NewPass/$token$'>
+            http://localhost:3000/teamProject#/NewPass/$token$
+           </a>
+     </div>`
+
 
 let instance = axios.create({
+    // baseURL: 'https://neko-back.herokuapp.com/2.0',
     baseURL: `http://localhost:7542/2.0/`,
     withCredentials: true,
 })
@@ -36,6 +46,17 @@ export const API = {
     },
     login(payload: LoginDataType) {
         return instance.post<LoginResponseType>(`auth/login`, payload)
+    },
+    forgotPassword(email: string) {
+        return instance.post('auth/forgot', {
+            email,
+            from: 'Microsoft<pharm.sale777@gmail.com>',
+            message: messageForRecoveryPassword
+        })
+    },
+    setNewPassword(password: string, resetPasswordToken: string) {
+        return instance.post<{ info: string }>('auth/set-new-password', {password, resetPasswordToken})
     }
+
 }
 
